d56a9e2c7306aca43f73dc0b456845da75e242aa
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-17 19:27:12 +0200

    💄    F02 : MENUNAV > Add Structure

--- src/component/pages/frontpage/navbar/menuNav/MenuNav.jsx
+++ src/component/pages/frontpage/navbar/menuNav/MenuNav.jsx
@@ -5,7 +5,7 @@ export default function MenuNav() {
   return (
     <MenuNavStyled>
       <ul>
-        <li>
+        <li className="top-border">
           <NavLink to="/menu">Menu</NavLink>
         </li>
         <li>
@@ -20,7 +20,7 @@ export default function MenuNav() {
         <li>
           <NavLink to="/blog">Blog</NavLink>
         </li>
-        <li>
+        <li className="bottom-border">
           <NavLink to="/styleguide">StyleGuide</NavLink>
         </li>
       </ul>
@@ -29,6 +29,45 @@ export default function MenuNav() {
 }
 
 const MenuNavStyled = styled.div`
-  border: 1px solid red;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: black;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+      width: auto;
+      margin-bottom: 20px;
+
+      a {
+        text-decoration: none;
+        color: white;
+        font-size: 50px;
+      }
+    }
+    .top-border {
+      border-top: 2px solid white;
+      width: 20%;
+      padding-top: 20px;
+      justify-content: center;
+      display: flex;
+    }
+
+    .bottom-border {
+      border-bottom: 2px solid white;
+      width: 20%;
+      padding-bottom: 20px;
+      justify-content: center;
+      display: flex;
+    }
+  }
 `;
