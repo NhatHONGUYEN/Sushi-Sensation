9cbc118984f9174dadab2e556b0f3498f82bb72c
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-17 14:41:27 +0200

    🍱  F02 : install style component

--- src/component/pages/frontpage/FrontPage.jsx
+++ src/component/pages/frontpage/FrontPage.jsx
@@ -1,3 +1,14 @@
 export default function FrontPage() {
-  return <div>FrontPage</div>;
+  return <div className="front-page">
+   {/* Navbar */}
+
+<div className="left-front-page">left-front-page</div>
+<div className="right-front-page">right-front-page</div>
+
+
+  </div>
 }
+
+const FrontPageStyled = styled.`
+  
+`;
