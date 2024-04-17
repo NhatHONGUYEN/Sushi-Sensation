0bca4cd1073f0ad140c1068cb0186c8e53bff927
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-17 14:30:11 +0200

    ✨ F02 : APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import FrontPage from "./component/pages/frontpage/FrontPage.jsx";
+import ErrorPage from "./component/pages/error/ErrorPage.jsx";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<FrontPage />} />
+      <Route path="/frontpage" element={<FrontPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
