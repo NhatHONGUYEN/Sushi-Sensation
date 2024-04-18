3b2d4e250433fc8dcddea5b23eacb66d459aa061
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-18 10:52:34 +0200

    💄    F02 : MENU > Add image for presentation menu image

--- src/component/pages/frontpage/menu/Menu.jsx
+++ src/component/pages/frontpage/menu/Menu.jsx
@@ -1,10 +1,41 @@
 import styled from "styled-components";
 
 export default function Menu() {
-  return <MenuStyled>menu</MenuStyled>;
+  return (
+    <MenuStyled>
+      <div className="presentation_menu_image">
+        <img
+          src="../../../../../public/image_bowl_left_menu.jpg"
+          alt="Image de présentation"
+        />
+      </div>
+      <div className="menu_items">menu_items</div>
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
   border: 1px solid red;
-  height: 100vh;
+  height: 97vh;
+  margin: 15px;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: center;
+
+  .presentation_menu_image {
+    background: red;
+    overflow: hidden;
+    max-width: 100%;
+    max-height: 100%;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: contain;
+    }
+  }
+  .menu_items {
+    background: blue;
+    height: 100%;
+  }
 `;
