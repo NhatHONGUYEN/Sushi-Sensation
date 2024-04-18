f286fadcd121ca8a686589a9c1a8a7493a475ef5
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-18 11:30:32 +0200

    🍱  F02 : fakeMenu > data

---
+++ src/fakedata/fakeMenu.js
@@ -0,0 +1,134 @@
+const menuItems = [
+  {
+    id: "1",
+    imageSource: "../../public/spicy_tuna_maki.jpg",
+    title: "SPICY TUNA MAKI",
+    description:
+      "A tantalizing blend of spicy tuna, cucumber, and avocado, harmoniously rolled in nori and seasoned rice.",
+    price: 5,
+  },
+  {
+    id: "2",
+    imageSource: "../../public/mango_maki.jpg",
+    title: "MANGO MAKI",
+    description:
+      "Tempura-fried shrimp, cucumber, and cream cheese embrace a center of fresh avocado, delivering a satisfying contrast of textures.",
+    price: 5,
+  },
+  {
+    id: "3",
+    imageSource: "../../public/salmon_maki.jpg",
+    title: "SALMON MAKI",
+    description:
+      "Shiitake mushrooms, avocado, and pickled daikon radish nestle within a roll of seasoned rice, coated with nutty sesame seeds.",
+    price: 5,
+  },
+  {
+    id: "4",
+    imageSource: "../../public/tuna_maki.jpg",
+    title: "TUNA MAKI",
+    description:
+      "A vibrant assortment of julienned carrots, bell peppers, and cucumber, tightly encased in a nori-wrapped rice roll.",
+    price: 5,
+  },
+  {
+    id: "5",
+    imageSource: "../../public/voclano_delight.jpg",
+    title: "VOLCANO DELIGHT",
+    description:
+      "Creamy crab salad, avocado, and cucumber rolled inside, topped with spicy tuna and drizzled with fiery sriracha sauce.",
+    price: 12,
+  },
+  {
+    id: "6",
+    imageSource: "../../public/rainbow_fusion.jpg",
+    title: "RAINBOW FUSION",
+    description:
+      "A colorful blend of fresh tuna, salmon, yellowtail, and avocado, enveloping a core of cucumber and crab stick.",
+    price: 12,
+  },
+  {
+    id: "7",
+    imageSource: "../../public/dragon_elegance.jpg",
+    title: "DRAGON ELEGANCE",
+    description:
+      "Grilled eel and avocado nestled within the roll, draped with slices of ripe avocado resembling dragon scales.",
+    price: 12,
+  },
+  {
+    id: "8",
+    imageSource: "../../public/sunset_serenity.jpg",
+    title: "SUNSET SERENITY",
+    description:
+      "Tempura shrimp, cucumber, and spicy mayo embraced by a roll of soy paper, crowned with slices of creamy mango.",
+    price: 12,
+  },
+  {
+    id: "9",
+    imageSource: "../../public/mystic_garden.jpg",
+    title: "MYSTIC GARDEN",
+    description:
+      "Shiitake mushrooms, asparagus, and cucumber intermingle with crispy tempura bits, blanketed by a layer of sesame seeds.",
+    price: 12,
+  },
+  {
+    id: "10",
+    imageSource: "../../public/ocean_breeze.jpg",
+    title: "OCEAN BREEZE",
+    description:
+      "A medley of fresh shrimp, crab stick, and avocado, laced with a gentle touch of zesty yuzu-infused tobiko.",
+    price: 12,
+  },
+  {
+    id: "11",
+    imageSource: "../../public/tokyo_blossom.jpg",
+    title: "TOKYO BLOSSOM",
+    description:
+      "Delicate pink soy paper envelopes a blend of tuna, crab stick, and cucumber, embellished with edible flower petals.",
+    price: 12,
+  },
+  {
+    id: "12",
+    imageSource: "../../public/sunrise_bliss.jpg",
+    title: "SUNRISE BLISS",
+    description:
+      "A delicate combination of fresh salmon, cream cheese, and asparagus, rolled in orange-hued tobiko for a burst of sunrise-inspired flavors.",
+    price: 16,
+  },
+  {
+    id: "13",
+    imageSource: "../../public/mango_tango.jpg",
+    title: "MANGO TANGO",
+    description:
+      "Tempura shrimp, cucumber, and avocado dance alongside sweet mango slices, drizzled with a tangy mango sauce.",
+    price: 16,
+  },
+  {
+    id: "14",
+    imageSource: "../../public/truffle_indulgence.jpg",
+    title: "TRUFFLE INDULGENCE",
+    description:
+      "Decadent slices of black truffle grace a roll of succulent wagyu beef, cucumber, and microgreens, culminating in an exquisite umami symphony.",
+    price: 16,
+  },
+  {
+    id: "15",
+    imageSource: "../../public/pacific_firecracker.jpg",
+    title: "PACIFIC FIRECRACKER",
+    description:
+      "Spicy crab salad, tempura shrimp, and jalapeño peppers combine in a fiery ensemble, accented with a chili-infused aioli.",
+    price: 16,
+  },
+  {
+    id: "16",
+    imageSource: "../../public/eternal_eel.jpg",
+    title: "ETERNAL EEL",
+    description:
+      "An enchanting blend of eel tempura, foie gras, and cucumber, elegantly layered with truffle oil and gold leaf for a touch of opulence.",
+    price: 16,
+  },
+];
+
+export const fakeMenu = {
+  menuItems,
+};
