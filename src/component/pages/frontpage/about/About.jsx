8d79a1780ac7eef9f3abc38b0e773eae64c5809f
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-19 08:04:16 +0200

    💄   F02 : MENU > Optimize perfomance > Change the size of pictures

--- src/component/pages/frontpage/about/About.jsx
+++ src/component/pages/frontpage/about/About.jsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+
 export default function About() {
-  return <div>OurStory</div>;
+  return (
+    <AboutStyled>
+      <div className="left-image-place">image place introduction</div>
+      <div className="our-story-descritpion">about</div>
+    </AboutStyled>
+  );
 }
+
+const AboutStyled = styled.div`
+  height: 97vh;
+  margin: 10px;
+  display: flex;
+  justify-content: space-around;
+
+  .left-image-place {
+    background: blue;
+    flex: 1;
+  }
+  .our-story-descritpion {
+    background: green;
+    flex: 1;
+  }
+`;
