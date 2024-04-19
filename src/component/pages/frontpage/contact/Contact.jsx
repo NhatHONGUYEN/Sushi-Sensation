06e30e6da016e197d995f56e53f56113be602856
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-19 16:10:47 +0200

    🚧    F03 : CONTACT > Start the layout

--- src/component/pages/frontpage/contact/Contact.jsx
+++ src/component/pages/frontpage/contact/Contact.jsx
@@ -1,3 +1,28 @@
+import styled from "styled-components";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <ContactStyled>
+      <div className="Contact">Contact</div>
+      <div className="info-for-contact">info-for-contact</div>
+    </ContactStyled>
+  );
 }
+
+const ContactStyled = styled.div`
+  background: red;
+  display: flex;
+  justify-content: space-around;
+  height: 97vh;
+  margin: 10px;
+  gap: 10px;
+
+  .Contact {
+    background: green;
+    width: 50%;
+  }
+  .info-for-contact {
+    background: blue;
+    width: 50%;
+  }
+`;
