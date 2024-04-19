98482f01a50a779ba73e994bb38801c783c93672
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-19 11:55:52 +0200

    🚧    F03 : ABOUT > work on the layout of the page

--- src/component/pages/frontpage/about/About.jsx
+++ src/component/pages/frontpage/about/About.jsx
@@ -1,10 +1,54 @@
 import styled from "styled-components";
+import imageForAbout from "../../../../../public/image_for_about.jpg";
+import womenWaiting from "../../../../../public/women_waiting.jpg";
+import cookingForAbout from "../../../../../public/cooking-for-about.jpg";
 
 export default function About() {
   return (
     <AboutStyled>
       <div className="image-for-about"></div>
-      <div className="our-story-descritpion">about</div>
+      <div className="our-story-description">
+        <div className="sushi-women-container">
+          <div className="sushi-artistry">
+            <h1>
+              SUSHI ARTISTRY <br />
+              REDEFINED
+            </h1>
+            <p>
+              Where culinary craftsmanship meets modern elegance. Indulge in the
+              finest sushi, expertly curated to elevate your dining experience.
+            </p>
+          </div>
+          <div>
+            <img src={womenWaiting} alt="women_waiting" />
+          </div>
+        </div>
+        <div className="trip-michelin-start-container">
+          <div className="trip-satisfaction">
+            TRIP ADVISOR <p>BEST SUSHI</p>
+          </div>
+          <div className="trip-satisfaction">
+            MiCHELIN GUIDE <p>QUALITY FOOD</p>
+          </div>
+          <div className="trip-satisfaction">
+            START DINING <p>COOL VIBE</p>
+          </div>
+        </div>
+        <div className="cooking-our-container">
+          <div>
+            <img src={cookingForAbout} alt="cooking-for-about" />
+          </div>
+          <div>
+            <h1>OUR STORY</h1>{" "}
+            <p>
+              Founded with a passion for culinary excellence, Qitchen's journey
+              began in the heart of Prague. Over years, it evolved into a haven
+              for sushi enthusiasts, celebrated for its artful mastery and
+              devotion to redefining gastronomy.
+            </p>
+          </div>
+        </div>
+      </div>
     </AboutStyled>
   );
 }
@@ -17,7 +61,7 @@ const AboutStyled = styled.div`
 
   .image-for-about {
     width: 50%;
-    background-image: url("../../../../../public/image_for_about.jpg");
+    background-image: url(${imageForAbout});
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
@@ -28,8 +72,68 @@ const AboutStyled = styled.div`
       display: none;
     }
   }
-  .our-story-descritpion {
+  .our-story-description {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     background: green;
     width: 50%;
+    padding: 20px;
+
+    .sushi-artistry {
+      border: 1px solid red;
+      height: 30vh;
+      width: 60%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 50px 30px;
+      border-radius: 20px;
+
+      h1 {
+        font-size: 25px;
+      }
+
+      p {
+        font-size: 16px;
+
+        color: #333;
+      }
+    }
+  }
+
+  .sushi-women-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    img {
+      height: 30vh;
+      border-radius: 20px;
+    }
+  }
+
+  .trip-michelin-start-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid blue;
+    height: 120px;
+    padding: 0 50px;
+
+    .trip-satisfaction {
+      border: 1px solid red;
+    }
+  }
+
+  .cooking-our-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  img {
+    height: 300px;
+    width: 300px;
   }
 `;
