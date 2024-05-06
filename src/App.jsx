fad29d4ef826f7648e35230083e1ea99de16e670
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-06 17:16:44 +0200

    💄  F11 :  ADD transition for the front page

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,37 @@ import FrontPage from "./component/pages/frontpage/FrontPage.jsx";
 import Menu from "./component/pages/frontpage/menu/Menu.jsx";
 import MenuNav from "./component/pages/frontpage/navbar/menuNav/MenuNav.jsx";
 import Reservation from "./component/pages/frontpage/reservation/Reservation.jsx";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const location = useLocation();
+  const [mount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
 
   return (
     <TransitionGroup>
-      <CSSTransition key={location.key} classNames="fade-right" timeout={1000}>
-        <Routes location={location}>
-          <Route path="/" element={<FrontPage />} />
-          <Route path="/frontpage" element={<FrontPage />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservation" element={<Reservation />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menuNav" element={<MenuNav />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </CSSTransition>
+      {mount && (
+        <CSSTransition
+          key={location.key}
+          classNames="fade-right"
+          timeout={1000}
+        >
+          <Routes location={location}>
+            <Route path="/" element={<FrontPage />} />
+            <Route path="/frontpage" element={<FrontPage />} />
+            <Route path="*" element={<ErrorPage />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/reservation" element={<Reservation />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/menuNav" element={<MenuNav />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </CSSTransition>
+      )}
     </TransitionGroup>
   );
 }
